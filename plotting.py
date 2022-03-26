b2dc86afc6cc2d410d3cc4874b7ada2c3b4d94d0
clear <fabian.kamp@fu-berlin.de>
2022-03-26 11:25:08 +0100

    added plotting

---
+++ plotting.py
@@ -0,0 +1,8 @@
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+from utils import *
+
+def plot_fcs(subject, group): 
+    
